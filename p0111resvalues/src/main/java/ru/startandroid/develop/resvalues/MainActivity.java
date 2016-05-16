3db4426a55a87c69d95445b450281a7598e7184c
Igor Andrianov <i.andrianov@fun-box.ru>
2016-05-17 00:44:25 +0300

    Lesson 11 completed.

---
+++ p0111resvalues/src/main/java/ru/startandroid/develop/resvalues/MainActivity.java
@@ -0,0 +1,69 @@
+package ru.startandroid.develop.resvalues;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+
+        btnBottom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.btnBottomText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnTop = (Button) findViewById(R.id.btnTop);
+        btnTop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.btnTopText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
