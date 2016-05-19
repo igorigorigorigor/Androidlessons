59ee82ac06b9cab50922810b0817f077d84b64ac
Igor Andrianov <i.andrianov@fun-box.ru>
2016-05-20 00:44:25 +0300

    Lesson 14 completed.

---
+++ p0141menuadv/src/main/java/ru/startandroid/develop/menuadv/MainActivity.java
@@ -0,0 +1,52 @@
+package ru.startandroid.develop.menuadv;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    CheckBox chb;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chb = (CheckBox) findViewById(R.id.chbExtMenu);
+        tv = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        menu.setGroupVisible(R.id.group1, chb.isChecked());
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        StringBuilder sb = new StringBuilder();
+
+        // Выведем в TextView информацию о нажатом пункте меню
+        sb.append("Item Menu");
+        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+        sb.append("\r\n title: " + item.getTitle());
+        tv.setText(sb.toString());
+
+        return super.onOptionsItemSelected(item);
+    }
+}
