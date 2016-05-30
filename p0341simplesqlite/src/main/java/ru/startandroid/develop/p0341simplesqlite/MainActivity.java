a66abacc69915b44c5af3be2d1ce133ad99032cc
Igor Andrianov <andrianov2406@gmail.com>
2016-05-31 00:32:53 +0300

    Lesson 35 completed.

--- p0341simplesqlite/src/main/java/ru/startandroid/develop/p0341simplesqlite/MainActivity.java
+++ p0341simplesqlite/src/main/java/ru/startandroid/develop/p0341simplesqlite/MainActivity.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     final String LOG_TAG = "MyLog";
     DBHelper dbHelper;
+    private Button btnUpdateID;
+    private Button btnDeleteID;
+    private EditText edID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +38,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnRead = (Button) findViewById(R.id.btnRead);
         btnClear = (Button) findViewById(R.id.btnClear);
+        btnUpdateID = (Button) findViewById(R.id.btnUpdateID);
+        btnDeleteID = (Button) findViewById(R.id.btnDeleteID);
+        edID = (EditText) findViewById(R.id.edID);
 
         btnAdd.setOnClickListener(this);
         btnRead.setOnClickListener(this);
         btnClear.setOnClickListener(this);
+        btnUpdateID.setOnClickListener(this);
+        btnDeleteID.setOnClickListener(this);
 
         dbHelper = new DBHelper(this);
     }
@@ -47,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ContentValues cv = new ContentValues();
         String name = etName.getText().toString();
         String email = etEmail.getText().toString();
+        String id = edID.getText().toString();
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         switch (v.getId()){
@@ -76,6 +86,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(LOG_TAG, "---Clear mytable: ----");
                 int clearCount = db.delete("mytable", null, null);
                 Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+            case R.id.btnUpdateID:
+                if (TextUtils.isEmpty(id)){
+                    break;
+                }
+                Log.d(LOG_TAG, "---Update mytable: ----");
+                cv.put("name", name);
+                cv.put("email", email);
+                int updateCount = db.update("mytable", cv, "id = ?", new String [] {id});
+                Log.d(LOG_TAG, "Updated rows count = " + updateCount);
+                break;
+            case R.id.btnDeleteID:
+                if (TextUtils.isEmpty(id)){
+                    break;
+                }
+                Log.d(LOG_TAG, "---Delete from mytable: ----");
+                int delCount = db.delete("mytable", "id = " + id, null);
+                Log.d(LOG_TAG, "Deleted rows count = " + delCount);
                 break;
         }
         dbHelper.close();
