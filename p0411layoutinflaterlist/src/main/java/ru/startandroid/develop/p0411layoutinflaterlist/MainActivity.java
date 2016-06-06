dbe579920b8781d22771752400c85de6a8f1b101
Igor Andrianov <andrianov2406@gmail.com>
2016-06-07 01:27:43 +0300

    Lesson 41 completed.

---
+++ p0411layoutinflaterlist/src/main/java/ru/startandroid/develop/p0411layoutinflaterlist/MainActivity.java
@@ -0,0 +1,53 @@
+package ru.startandroid.develop.p0411layoutinflaterlist;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь" };
+    String[] position = { "Программер", "Бухгалтер", "Программер",
+            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник" };
+    int salary[] = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 };
+
+    int[] colors = new int[2];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        colors[0] = Color.parseColor("#559966CC");
+        colors[1] = Color.parseColor("#55336699");
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linLayout);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        for (int i = 0; i < name.length; i++){
+            View item = layoutInflater.inflate(R.layout.item, linearLayout, false);
+
+            TextView tvName = (TextView) item.findViewById(R.id.tvName);
+            tvName.setText(name[i]);
+            TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
+            tvPosition.setText(position[i]);
+            TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
+            tvSalary.setText(String.valueOf(salary[i]));
+            item.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+            item.setBackgroundColor(colors[i%2]);
+            linearLayout.addView(item);
+        }
+    }
+}
